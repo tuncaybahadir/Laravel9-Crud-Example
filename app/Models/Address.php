9e1d35b00af94426a7206df4f49aaaf8d0c9e4c8
tuncay bahadır <tuncaybahadir@yandex.com.tr>
2022-09-07 00:49:26 +0300

    Attributes were added to the address model and the fillable array was updated.

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
@@ -9,10 +10,41 @@ class Address extends Model
     public $timestamps = false;
 
     protected $fillable = [
+        'person_id',
         'address',
         'post_code',
         'city_name',
         'country_name'
     ];
+
+    /**
+     * @return Attribute
+     */
+    protected function cityName(): Attribute
+    {
+        return Attribute::make(
+            set: static fn ($value) => tr_ucfirst_all($value),
+        );
+    }
+
+    /**
+     * @return Attribute
+     */
+    protected function countryName(): Attribute
+    {
+        return Attribute::make(
+            set: static fn ($value) => tr_ucfirst_all($value),
+        );
+    }
+
+    /**
+     * @return Attribute
+     */
+    protected function address(): Attribute
+    {
+        return Attribute::make(
+            set: static fn ($value) => tr_ucfirst_all($value),
+        );
+    }
 
 }
